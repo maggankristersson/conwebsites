d1d7930ea10b32627ed718a830fc1d07b7fcb8a9
snabelbabbsan <120466826+snabelbabbsan@users.noreply.github.com>
2023-02-21 12:16:06 +0100

    random shit

---
+++ Con_Art/char.js
@@ -0,0 +1,13 @@
+let animation = anime({
+    targets: '.letter',
+    opacity: 1,
+    translateY: 50, 
+    rotate: {
+      value: 360,
+      duration: 2000,
+      easing: 'easeInExpo'
+    }, 
+    scale: anime.stagger([0.7, 1], {from: 'center'}), 
+    delay: anime.stagger(100, {start: 1000}), 
+    translateX: [-10, 30]
+  });    
